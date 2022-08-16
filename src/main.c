f38f5a91bcc604c22f1282362b052084d0760f75
LeonelMatos <leonellmts@gmail.com>
2022-08-16 22:04:38 +0100

    Updated Makefile and others

--- src/main.c
+++ src/main.c
@@ -84,8 +84,13 @@ int getWindowSize(int *rows, int *cols)
 {
     struct winsize ws;
 
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        //Moves the cursor forward/down to the down-right corner of the terminal
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        editorReadKey();
         return -1;
+    }
     else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
