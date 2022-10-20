53a67ceb42f72bc9b968da898594fd0d7f8cb912
LeonelMatos <leonellmts@gmail.com>
2022-10-20 18:20:12 +0100

    Horizontal scrolling

--- src/main.c
+++ src/main.c
@@ -50,8 +50,11 @@ struct editorConfig
 {
     /// cx, cy: current position of the cursor
     int cx, cy;
-    /// Row of the file the user scrolled to
+    // Row of the file the user scrolled to
+    /// Offset related to the row of text
     int rowoff;
+    /// Offset related to the column of text
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -307,6 +310,13 @@ void editorScroll()
     if (E.cy >= E.rowoff + E.screenrows) {
         E.rowoff = E.cy - E.screenrows + 1;
     }
+
+    if (E.cx < E.coloff) {
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 /// Draws the rows on the side of the editor
@@ -345,9 +355,10 @@ void editorDrawRows(struct abuf *ab)
                 abAppend(ab, "*", 1);
             }
         } else {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[filerow].chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
 
         // abAppend(ab, "*", 1);
@@ -374,7 +385,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff), E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -397,7 +408,6 @@ void editorMoveCursor(int key)
             E.cx--;
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1)
             E.cx++;
         break;
     case ARROW_UP:
@@ -460,6 +470,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
